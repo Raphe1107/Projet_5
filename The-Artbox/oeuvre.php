5edb11757e5696994ba91231bb6d9aff64d5621c
Raphaël NUGUET <rnuguet@gmail.com>
2023-03-29 17:54:46 +0200

    Début projet 5

---
+++ The-Artbox/oeuvre.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>The ArtBox</title>
+</head>
+<body>
+
+
+    <?php include_once "header.php"; ?>
+
+    <main>
+        <?php include_once "oeuvres.php"; ?>
+            <article id="detail-oeuvre">
+            <div id="img-oeuvre">
+            <img src="img/clark-van-der-beken.png" alt="Dodomu">
+            </div>
+            <div id="contenu-oeuvre">
+            <h1></h1>
+            <p class="description"></p>
+            <p class="description-complete">
+            </div>
+            </article>
+
+
+
+
+
+    </main>
+
+    <?php include_once "footer.php"; ?>
+
+
+</body>
